50edc853bf0df91e38d6ae7fed0c29f2c7321bb5
Cuthbert Guerrero <cuthbert.guerrero@gmail.com>
2022-05-15 16:07:39 +0800

    Done with ch11

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"runtime/debug"
 	"time"
+
+	"github.com/justinas/nosurf"
 )
 
 func (app *application) serverError(w http.ResponseWriter, err error) {
@@ -27,9 +29,15 @@ func (app *application) addDefaultData(td *templateData, r *http.Request) *templ
 	if td == nil {
 		td = &templateData{}
 	}
+	td.CSRFToken = nosurf.Token(r)
 	td.CurrentYear = time.Now().Year()
 	td.Flash = app.session.PopString(r, "flash")
+	td.IsAuthenticated = app.isAuthenticated(r)
 	return td
+}
+
+func (app *application) isAuthenticated(r *http.Request) bool {
+	return app.session.Exists(r, "authenticatedUserID")
 }
 
 func (app *application) render(w http.ResponseWriter, r *http.Request, name string, td *templateData) {
